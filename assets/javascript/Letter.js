f19208620f0d0dc08565abf92472b468756ffb50
Rebecca Hom <rebecca.hom001@gmail.com>
2018-12-10 22:32:16 -0800

    optimized Letter constructor

--- assets/javascript/Letter.js
+++ assets/javascript/Letter.js
@@ -5,16 +5,14 @@ var Letter = function(char, guessed = false) {
 
     // Returns the character or a placeholder depending on if the letter has been guessed.
     this.toString = () => {
-        if(this.guessed) {
-            return this.char;
-        }
-
+        if(this.guessed) return this.char;
+        
         return "_";
     }
 
     // Updates this.guessed to true if the character matches.
     this.guessing = (guess) => {
-        if(this.char.toLowerCase() == guess.toLowerCase()) { this.guessed = true; }
+        if(this.char.toLowerCase() == guess.toLowerCase()) this.guessed = true;
         return this.guessed;
     }
 }
