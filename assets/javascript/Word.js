7e8c97094e64cd04f85389fa86df93b20cc31966
Rebecca Hom <rebecca.hom001@gmail.com>
2018-12-10 17:37:03 -0800

    added constructor for Word

---
+++ assets/javascript/Word.js
@@ -0,0 +1,42 @@
+var Letter = require("./Letter.js");
+
+var Word = function(phrase) {
+    this.phrase = phrase,   // The phrase is an array of Letters.
+
+    // Returns a string representing the word from the Letters.
+    this.toWord = () => {
+        var phrase = "";
+        for(var i = 0; i < this.phrase.length; i++) {
+            phrase += this.phrase[i].toString();
+
+            if(i < this.phrase.length - 1) {
+                phrase += " ";
+            }
+        }
+
+        return phrase;
+    },
+
+    // Calls the guess function for each letter in the phrase.
+    // Returns true if the letter is in the word.
+    this.guess = (char) => {
+        var guessed = false;
+        var str = this.toWord();
+
+        // If the letter already exists in the word, then don't run the function
+        if(str.indexOf(char) >= 0) {
+            return true;
+        }
+
+        for(var i = 0; i < this.phrase.length; i++) {
+            // Updates the letter if it has been guessed.
+            if(this.phrase[i].guessing(char)) {
+                guessed = true;
+            }
+        }
+
+        return guessed;
+    }
+}
+
+module.exports = Word;
