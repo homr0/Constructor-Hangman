f16c4975fcc2e6cce0edb0c368593ec5d725c235
Rebecca Hom <rebecca.hom001@gmail.com>
2018-12-10 16:36:37 -0800

    added constructor for Letter

---
+++ assets/javascript/Letter.js
@@ -0,0 +1,23 @@
+var Letter = (char, guessed) => {
+    // Properties
+    this.char = char,       // Underlying character
+    this.guessed = guessed, // Boolean if letter has been guessed.
+
+    // Returns the character or a placeholder depending on if the letter has been guessed.
+    this.toString = () => {
+        if(this.guessed) {
+            return this.char;
+        }
+
+        return "_";
+    }
+
+    // Updates this.guessed to true if the character matches.
+    this.guessing = (guess) => {
+        this.guessed = (this.char === guess);
+    }
+}
+
+module.exports = {
+    Letter: Letter
+}
